8ecddc119f6cb499f15c22a3c70af46e4ff46600
desbain <desbaina@yahoo.com>
2024-09-16 21:58:51 -0700

    file7

--- main.tf
+++ main.tf
@@ -26,7 +26,7 @@ resource "aws_instance" "server" {
   ami             = "ami-0aa2b7722dc1b5612" # Ubuntu AMI ID
   instance_type   = "t2.medium"             # Change this to your preferred instance type
   key_name = "DesbainKP"
-  count = 2
+  count = 1
 
   // Associate the security group with the instance
   security_groups = [aws_security_group.my-sg.name]
